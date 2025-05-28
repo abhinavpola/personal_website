bf382dd17ad8ee6bb03555dd647470cb80564fbb
Abhinav Pola <abhinavpola7@gmail.com>
2025-05-28 10:03:44 -0700

    turn this into a nextjs project

---
+++ config/site.ts
@@ -0,0 +1,34 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Next.js + HeroUI",
+  description: "Make beautiful websites regardless of your design experience.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Resume",
+      href: "/resume",
+    },
+    {
+      label: "Projects",
+      children: [
+        {
+          label: "Agent Safety Evals",
+          href: "/agent-safety-evals",
+        },
+        {
+          label: "Crowdsourced Fractal Computation",
+          href: "/crowdsourced-fractal-computation",
+        },
+      ],
+    },
+  ],
+  links: {
+    github: "https://github.com/abhinavpola/personal_website",
+    twitter: "https://x.com/abhinavpola",
+    linkedin: "https://www.linkedin.com/in/abhinav-pola/",
+  },
+};
